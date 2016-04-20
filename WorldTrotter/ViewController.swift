f52f49a479fc47332bfcc351ebd7e1a3aa2ef392
Nathan Gladson <nathan@egovtn.org>
2016-04-20 16:44:06 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Nathan Gladson on 4/20/16.
+//  Copyright © 2016 nmg82. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
